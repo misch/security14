0175f3d90ba7dc212a87d8b92737fbc6a3f1cbde
Michèle Wyss <michele_wyss@students.unibe.ch>
2014-12-03 17:45:50 +0100

    Reworked finding host and passing data to the true http server. Most sites working now!

--- httpProxy/src/httpProxy/ProxyThread.java
+++ httpProxy/src/httpProxy/ProxyThread.java
@@ -12,96 +12,94 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProxyThread extends Thread {
-    private Socket socket = null;
-    
-    public ProxyThread(Socket socket) {
-        super();
-        this.socket = socket;
-    }
+	private Socket socket = null;
 
-    public void run() {
-    	try {
-    		
-            DataOutputStream browserOut = new DataOutputStream(socket.getOutputStream());
-            BufferedReader browserIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	public ProxyThread(Socket socket) {
+		super();
+		this.socket = socket;
+	}
 
-            String host = "";
-            int port = 80;
+	public void run() {
+		try {
 
-            Socket httpServerSocket = null; 
-            
-            InputStream serverIn = null;
-            PrintWriter serverOut = null;
-            
-            String inputLine;
-            while ((inputLine = browserIn.readLine()) != null) {
-            	if (inputLine.isEmpty()){
-            		break;
-            	}
-            	
-            	/* Get url out of the request */
-            	if(inputLine.split(" ")[0].equals("GET")){
-            		String url = inputLine.split(" ")[1];
-            		host = getHost(url);
-            		httpServerSocket = new Socket(host,port);
-            		serverOut = new PrintWriter(httpServerSocket.getOutputStream());
-            	}
-            	serverOut.println(inputLine + "\r\n");
-            }
-            
-            serverOut.flush();
-            
-            serverIn = httpServerSocket.getInputStream();
-            boolean blocked = hostIsBlocked(host);
-            if (blocked){
-            	sendURLBlockedMessage(browserOut);
-            }else{
-            	final int BUFFER_SIZE = 32768;
-                byte by[] = new byte[ BUFFER_SIZE ];
-                int index = serverIn.read( by, 0, BUFFER_SIZE );
-                while ( index != -1 )
-                {
-                  browserOut.write( by, 0, index );
-                  index = serverIn.read( by, 0, BUFFER_SIZE );
-                }
-                browserOut.flush();
-                browserOut.close();
-            }
-    	}catch(Exception e){
-    		e.printStackTrace();
-    	}
-    }
-    
-    private String getHost(String urlToCall) {
+			DataOutputStream browserOut = new DataOutputStream(
+					socket.getOutputStream());
+			BufferedReader browserIn = new BufferedReader(
+					new InputStreamReader(socket.getInputStream()));
+
+			String GETLine = browserIn.readLine();
+			while (GETLine != null && !GETLine.contains("GET")) {
+				GETLine = browserIn.readLine();
+			}
+			String url = GETLine.split(" ")[1];
+			String host = getHost(url);
+
+			if (hostIsBlocked(host)) {
+				sendURLBlockedMessage(browserOut);
+				return;
+			}
+
+			Socket httpServerSocket = new Socket(host, 80);
+
+			InputStream serverIn = httpServerSocket.getInputStream();
+			PrintWriter serverOut = new PrintWriter(httpServerSocket.getOutputStream());
+
+			serverOut.println(GETLine + "\r\n");
+
+			String inputLine;
+			while ((inputLine = browserIn.readLine()) != null) {
+				if (inputLine.isEmpty()) {
+					break;
+				}
+				serverOut.println(inputLine + "\r\n");
+			}
+			serverOut.flush();
+
+			final int BUFFER_SIZE = 32768;
+			byte by[] = new byte[BUFFER_SIZE];
+			int index = serverIn.read(by, 0, BUFFER_SIZE);
+			while (index != -1) {
+				browserOut.write(by, 0, index);
+				index = serverIn.read(by, 0, BUFFER_SIZE);
+			}
+			browserOut.flush();
+			browserOut.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String getHost(String urlToCall) {
 		String host = urlToCall.split("/")[2];
 		return host;
 	}
 
-	private void sendURLBlockedMessage(DataOutputStream browserOut){
-    	try {
+	private void sendURLBlockedMessage(DataOutputStream browserOut) {
+		try {
 			browserOut.write("HTTP/1.1 403 Forbidden\r\n".getBytes());
-	    	browserOut.write("Content-Type: text/plain; charset=UTF-8\r\n".getBytes());
-	    	browserOut.write("\r\n".getBytes());
-	    	browserOut.write("This content is sooo blocked by the proxy...\r\n".getBytes());
-	    	browserOut.flush();
-	    	browserOut.close();
+			browserOut.write("Content-Type: text/plain; charset=UTF-8\r\n"
+					.getBytes());
+			browserOut.write("\r\n".getBytes());
+			browserOut.write("This content is sooo blocked by the proxy...\r\n"
+					.getBytes());
+			browserOut.flush();
+			browserOut.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
 
-    }
-    
-    private boolean hostIsBlocked(String host){
-    	List<String> blacklist = readFile("blacklist.txt");
-    	
-    	for (String badURL : blacklist) {
-    			if(host.matches(badURL)){
-    				return true;
-    			}
-    	}
-    	return false;
-    }
-    
+	private boolean hostIsBlocked(String host) {
+		List<String> blacklist = readFile("blacklist.txt");
+
+		for (String badURL : blacklist) {
+			if (host.matches(badURL)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	private List<String> readFile(String filename) {
 		String path = System.getProperty("user.dir");
 		path += "\\" + filename;
